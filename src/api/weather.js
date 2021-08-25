4a3a8e4deddbec1a167aade079bc2105a453c598
Maneet Lodha <36129353+maneetlodha28@users.noreply.github.com>
2021-08-25 15:05:25 +0530

    Http => https for api

--- src/api/weather.js
+++ src/api/weather.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const weatherAPI = async(query)=>{
     const apiKey='29754ae96b7459924b8b717f1e9997e6'
-    const URL=`http://api.openweathermap.org/data/2.5/weather`
+    const URL=`https://api.openweathermap.org/data/2.5/weather`
 
      const {data} = await axios.get(URL, {
          params:{
